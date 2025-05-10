420aaaf586131b10e04577a4ea5b6658b9d01b2e
Anvesh-rio <jerupatianvesh01@gmail.com>
2025-05-10 12:14:54 +0530

    new

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(cors({ origin: "*" }));
 app.use(express.urlencoded({ extended: true }));
 
 
-const db = "mongodb+srv://anvesh:5jcoDMx2xyRrU2qV%21%40@cluster0.0c4egmk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const db = process.env.MONGODB_URI;
 const port = process.env.PORT || 8889;
 
 
@@ -19,15 +19,26 @@ const adminRoutes = require('./routes/admin');
 app.use('/api/user', userRoutes);
 app.use('/api/admin', adminRoutes);
 
-mongoose
-  .set("strictQuery", true)
-  .connect(db, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => console.log("MongoDB successfully connected"))
-  .catch((err) => console.log("Error connecting to MongoDB", err));
+const connectWithRetry = () => {
+  console.log("Attempting MongoDB connection...");
 
+  mongoose
+    .set("strictQuery", true)
+    .connect(db, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(() => {
+      console.log("MongoDB successfully connected");
+    })
+    .catch((err) => {
+      console.error("MongoDB connection error:", err.message);
+      console.log("Retrying in 5 seconds...");
+      setTimeout(connectWithRetry, 5000);
+    });
+};
+
+connectWithRetry();
 
 app.listen(port, () => console.log(`Server up and running on port ${port} !`));
 
